3fe20ae785e20e4b5ecb8ad27371d5ebd8ccf9a4
marciotorres <marciotorres8888@gmail.com>
2022-10-13 13:44:18 -0300

    fase1

---
+++ src/main/java/one/digitalinovation/clouldparking/ClouldparkingApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinovation.clouldparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClouldparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClouldparkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinovation/clouldparking/ClouldparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinovation.clouldparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClouldparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
